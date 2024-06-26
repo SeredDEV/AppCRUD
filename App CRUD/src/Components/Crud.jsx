d6f4c33918d82ed81f6c1cb4aa9062e02895d21c
Sergio Rodriguez <sergiomorales9510@gmail.com>
2024-06-26 17:53:04 -0500

    update

--- App CRUD/src/Components/Crud.jsx
+++ App CRUD/src/Components/Crud.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import uuid from 'react-uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Crud = () => {
   // Create;
@@ -13,61 +14,51 @@ const Crud = () => {
   const [edit, setEdit] = useState(false);
 
   const addTask = () => {
-    const task = {id:uuid(), title, description };
-    setTasks([...task, task]);
-    clear()
+    const task = { id: uuid(), title, description };
+    setTasks([...tasks, task]);
+    clear();
   };
 
-
-  const updateTask=()=>{
-
-const id=localStorage.getItem("id")
-const newTask={id,title,description}
-const newTasks=tasks.map(item=>item.id===id?newTask:item)
-setTasks(newTasks)
-clear();
-
-  }
+  const updateTask = () => {
+    const id = localStorage.getItem("id");
+    const newTask = { id, title, description };
+    const newTasks = tasks.map(item => item.id === id ? newTask : item);
+    setTasks(newTasks);
+    clear();
+  };
 
   const actions = (e) => {
     e.preventDefault();
-    edit?updateTask():addTask
-    addTask();
+    edit ? updateTask() : addTask();
   };
 
-  const deleteTask=(id)=>{
+  const deleteTask = (id) => {
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks);
+  };
 
-const newTasks=tasks.filter(task=>task.id!==id)
-setTasks(newTasks)
+  const getData = (id) => {
+    const task = tasks.find(item => item.id === id);
+    localStorage.setItem("id", id);
+    setTitle(task.title);
+    setDescription(task.description);
+    setEdit(true);
+  };
 
-  }
-
-
-  const getData=(id)=>{
-
-const task=tasks.find(item=>item.id===id)
-localStorage.setItem("id",id)
-setTitle(task.title)
-setDescription(task.description)
-setEdit(true)
-
-  }
-
-  const clear=()=>{
-
-setTitle('');
-setDescription('');
-setEdit(false);
-
-
-  }
+  const clear = () => {
+    setTitle('');
+    setDescription('');
+    setEdit(false);
+  };
 
   return (
-    <div className="container">
-      <div className="mt-5 justify-content-center d-flex align-items-center">
-        <div className="col-6">
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
           <div className="card">
-            <h3 className="card-title text-center">CRUD</h3>
+            <div className="card-header text-center">
+              <h3>CRUD</h3>
+            </div>
             <div className="card-body">
               <form onSubmit={actions}>
                 <div className="mb-3">
@@ -91,29 +82,30 @@ setEdit(false);
                     onChange={(e) => setDescription(e.target.value)}
                   />
                 </div>
-                <button className="btn btn-primary font-control" type="submit">
-                  Save
+                <button className="btn btn-primary w-100" type="submit">
+                  {edit ? "Update" : "Save"}
                 </button>
               </form>
             </div>
           </div>
+          <ul className="list-group list-group-numbered mt-3">
+            {tasks.map((task) => (
+              <li className="list-group-item d-flex justify-content-between align-items-start" key={task.id}>
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">{task.title}</div>
+                  {task.description}
+                </div>
+                <button className="btn btn-danger me-2" onClick={() => deleteTask(task.id)}>
+                  <i className="fa-regular fa-trash-can"></i>
+                </button>
+                <button className="btn btn-warning" onClick={() => getData(task.id)}>
+                  <i className="fa-regular fa-pen-to-square"></i>
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-
-      {/* list task */}
-
-      <ul className="list-group list-group-numbered">
-        {tasks.map((task) => (
-          <li className="list-group-item d-flex justify-content-between align-items-start" key={task.id}>
-            <div className="ms-2 me-auto">
-              <div className="fw-bold">{task.title}</div>
-              {task.description}
-            </div>
-            <button className="btn btn-danger me-3"><i className="fa-regular fa-trash-can" onClick={()=>deleteTask(task.id)}></i></button>
-            <button className="btn btn-warning" onClick={()=>getData(task.id)}><i className="fa-regular fa-pen-to-square"></i></button>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
